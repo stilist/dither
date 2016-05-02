00bd9968a85e877c3e66a17e018149e33a329f03
Jordan Cole <stilist@users.noreply.github.com>
2016-05-02 08:02:51 -0700

    Update application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,15 +2,14 @@
   var img = new Image()
   img.src = 'rhino.jpg'
 
-  var surface = document.getElementById('source').
-    getContext('2d')
-
   var ditherers = [
     Dither.Atkinson,
     Dither.FloydSteinberg
   ]
 
   img.onload = function () {
+    var surface = document.getElementById('source').
+      getContext('2d')
     surface.drawImage(img, 0, 0)
     img.style.display = 'none'
 
