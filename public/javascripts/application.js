1978dfba1f6741e0317f9de7c5eb21b9f7a8d394
Jordan Cole <stilist@ratafia.info>
2016-05-05 20:26:42 -0700

    Split up image-load callback

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,21 +3,16 @@
 var DitherApplication = function () {
   this.current_image = 'rhino.jpg'
 
-  this.render()
+  this.update()
 }
-DitherApplication.prototype.render = function () {
+
+DitherApplication.prototype.update = function () {
   var img = new Image()
   img.src = this.current_image
 
   var _t = this
   img.onload = function () {
-    var source = document.getElementById('source')
-    source.height = img.height
-    source.width = img.width
-
-    var surface = source.getContext('2d')
-    surface.drawImage(img, 0, 0)
-    img.style.display = 'none'
+    _t.renderSource(this)
 
     var len = _t.algorithms.
       length
@@ -26,6 +21,17 @@ DitherApplication.prototype.render = function () {
     }
   }
 }
+
+DitherApplication.prototype.renderSource = function (img) {
+  var source = document.getElementById('source')
+  source.height = img.height
+  source.width = img.width
+
+  var surface = source.getContext('2d')
+  surface.drawImage(img, 0, 0)
+  img.style.display = 'none'
+}
+
 DitherApplication.prototype.algorithms = [
   Dither.Threshold,
   Dither.Naive,
